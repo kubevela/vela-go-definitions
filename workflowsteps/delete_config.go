05dc29a2b88d7938537074260f4274a1deb05c12
Ayush Kumar <ayushshyamkumar888@gmail.com>
2026-03-13 19:29:26 +0530

    fix(workflowsteps): refactor CreateConfig and DeleteConfig to use full parameter object and add tests for CreateConfig WorkflowStep
    Signed-off-by: Ayush Kumar <ayushshyamkumar888@gmail.com>

--- workflowsteps/delete_config.go
+++ workflowsteps/delete_config.go
@@ -38,10 +38,7 @@ func DeleteConfig() *defkit.WorkflowStepDefinition {
 		Params(name, namespace).
 		Template(func(tpl *defkit.WorkflowStepTemplate) {
 			tpl.Builtin("deploy", "config.#DeleteConfig").
-				WithParams(map[string]defkit.Value{
-					"name":      name,
-					"namespace": namespace,
-				}).
+				WithFullParameter().
 				Build()
 		})
 }
